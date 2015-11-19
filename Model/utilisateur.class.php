a973676c2bfafec290019bef4e7a9ac3f956b874
SuperOuf <le.super.ouf@gmail.com>
2015-11-19 14:54:03 +0100

    Encodage mdp

--- Model/utilisateur.class.php
+++ Model/utilisateur.class.php
@@ -8,10 +8,38 @@ class Utilisateur {
   var $siteWeb;
   var $tel;
   var $mail;
+  var $mdp;
 
   var $zone;
 
   // Association avec lui-même avec les contacts
   var $contacts;
+
+
+  public login($mail,$mdp){
+  	if ($mail != NULL && $mdp !=NULL) {
+            try {
+            	$mdp = password($mdp);
+              	$q = "SELECT email,mdp FROM Utilisateur WHERE mail=$mail and mdp=$mdp";
+              	$r = $this->db->exec($q);
+              	if ($r == 0) {
+              	return false;
+              }
+              
+            } catch (PDOException $e) {
+            
+            }
+    } else {
+    	return false;
+    }
+
+  }
+
+    private function password($mdp){
+    	$salt = "9W5NXle5691ah08tWKz02DJB40E6XFU3";
+    	$mdp = md5($mdp . $salt);
+    	return $mdp;
+	}
+
 }
  ?>
